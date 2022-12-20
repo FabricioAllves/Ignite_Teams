c48a9a6f5625aff84c8efdbbfcf2eaec791e760a
fabricio-26 <fabricioohh@gmail.com>
2022-12-20 11:16:31 -0300

    Feat: statusBar estilizado

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,15 @@
+import { StatusBar } from 'react-native';
 import React from 'react';
 import { Container, LoadIndicator } from './styles';
 
-export function Loading(){
-  return(
+export function Loading() {
+  return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <LoadIndicator />
     </Container>
   )
