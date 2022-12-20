889b1248801e6040ab69fd735b46fc5418c70230
fabricio-26 <fabricioohh@gmail.com>
2022-12-20 09:16:00 -0300

    Feat: criado componente de loading e tambem personalizaçao de fontes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { Groups } from '@pages/Groups';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading';
+
 import theme from './src/theme';
 
+import { Groups } from '@pages/Groups';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
+      { fontsLoaded ? <Groups/> : <Loading />}
     </ThemeProvider>
   );
 }
