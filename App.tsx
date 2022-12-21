8f63f138e0c998903c8bf0c1d74209d15bc1492e
fabricio henrique alves <fabricioohh@gmail.com>
2022-12-20 23:56:15 -0300

    Feat: componente GroupCard criado e estilizado, listando grupos com FlatList e componente de List Empty caso a lista esteja vazia

--- App.tsx
+++ App.tsx
@@ -6,12 +6,18 @@ import { Loading } from '@components/Loading';
 import theme from './src/theme';
 
 import { Groups } from '@pages/Groups';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+      barStyle='light-content'
+      backgroundColor="transparent"
+      translucent
+      />
       { fontsLoaded ? <Groups/> : <Loading />}
     </ThemeProvider>
   );
